e8e44c1caa2c7afbe4d038d1fb7374ac7ec7b3d1
ellysus <eliaselasri00@gmail.com>
2020-07-29 16:02:31 +0100

    improve bot
    -Bot Now uses RegEx instead of Array pattern that caused signals missing
    -Bot shows all Forex pairs(Majors, Minors, Exotics) and Metals after logging in
    -Bot shows Order details after placing
    -Added MetaTrader Credentials to config.ini

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from credentials import acc_login, acc_password, acc_server
 from time import sleep
 import configparser
 import re
@@ -27,6 +26,11 @@ api_hash = str(api_hash)
 
 phone = config['Telegram']['phone']
 username = config['Telegram']['username']
+signal_channel = config['Telegram']['signal_channel']
+
+acc_login = config['MetaTrader']['acc_login']
+acc_password = config['MetaTrader']['acc_password']
+acc_server = config['MetaTrader']['acc_server']
 
 class MTBot():
     def __init__(self):
@@ -35,8 +39,9 @@ class MTBot():
         self.driver = webdriver.Firefox(profile)
 
     def login(self):
+        print('MT4-Telegram Trade Copy Bot\n')
         self.driver.get('https://trade.mql5.com/')
-
+        print('Logging In...\n')
         sleep(2)
 
         usrname_in = self.driver.find_element_by_xpath('//*[@id="login"]')
@@ -51,6 +56,41 @@ class MTBot():
 
         login_btn = self.driver.find_element_by_xpath('/html/body/div[14]/div/div[3]/button[2]')
         login_btn.click()
+
+        sleep(5)
+
+        print('Showing All Symbols...')
+        symbols_menu = self.driver.find_element_by_xpath('/html/body/div[3]/div[1]/a[2]')
+        symbols_menu.click()
+
+        sleep(1)
+
+        FX_MAJORS = self.driver.find_element_by_xpath('/html/body/div[16]/div/div[3]/div/div/div/div[2]/div[2]')
+        FX_MINORS = self.driver.find_element_by_xpath('/html/body/div[16]/div/div[3]/div/div/div/div[2]/div[3]')
+        FX_EXOTICS = self.driver.find_element_by_xpath('/html/body/div[16]/div/div[3]/div/div/div/div[2]/div[4]')
+        METALS = self.driver.find_element_by_xpath('/html/body/div[16]/div/div[3]/div/div/div/div[2]/div[8]')
+        
+        show_btn = self.driver.find_element_by_xpath('/html/body/div[16]/div/div[3]/button[1]')
+        close_btn = self.driver.find_element_by_xpath('/html/body/div[16]/div/div[3]/button[4]')
+
+        FX_MAJORS.click()
+        show_btn.click()
+        sleep(0.1)
+        FX_MINORS.click()
+        show_btn.click()
+        sleep(0.1)
+        FX_EXOTICS.click()
+        show_btn.click()
+        sleep(0.1)
+        METALS.click()
+        show_btn.click()
+        sleep(0.1)
+
+        close_btn.click()
+
+        print('Bot ready!\nAwaiting signals...')
+
+
     
     def place_order(self, symbol, buysell, tp, sl):
         order_btn = self.driver.find_element_by_xpath('/html/body/div[3]/div[1]/a[1]')
@@ -94,17 +134,15 @@ async def main():
     print('Client Started.')
 
 
-@client.on(events.NewMessage(chats=-1001262107919))
+@client.on(events.NewMessage(chats=int(signal_channel)))
 async def signal_handler(event):
     print('New Signal!')
     signal = event.text
 
-    trade_data = signal.split()
-
-    symbol = trade_data[0]
-    order_type = trade_data[1]
-    stop_loss_str = trade_data[5]
-    take_profit_str = trade_data[7]
+    symbol = re.findall(r"([^\s]+)", signal)[0].upper()
+    order_type = re.findall(r"(buy|sell)", signal, re.I)[0].lower()
+    stop_loss_str = re.findall(r"(?<=Sl.)[0-9].[0-9]+", signal, re.I)[0]
+    take_profit_str = re.findall(r"(?<=tp.)[0-9].[0-9]+|(?<=target.)[0-9].[0-9]+", signal, re.I)[0]
 
     try:
         stop_loss = float(stop_loss_str)
@@ -116,6 +154,7 @@ async def signal_handler(event):
 
     if trade_is_valid == True:
         bot.place_order(symbol, order_type, take_profit, stop_loss)
+        print('Order Placed!\n', symbol, order_type, '\n SL: ', stop_loss, '\n TP: ', take_profit)
     elif trade_is_valid == False:
         print("Order data not found... Disregarding")
     else:
@@ -125,6 +164,3 @@ async def signal_handler(event):
 with client:
     client.start()
     client.run_until_disconnected()
-
-#bot.place_order("Eurusd", 1.1540, 1.1800, "sell")
-
